adffd0d876dc3e3a7668ced953a325f0ddd19621
koybasimuhittin <koybasimuhittin@gmail.com>
2024-12-06 17:33:41 +0300

    add block class

---
+++ block.py
@@ -0,0 +1,13 @@
+class Block:
+    def __init__(self, factions, top_left, bottom_right, block_id, process_rank, adjacent_blocks):
+        self.factions = factions
+        self.top_left = top_left
+        self.bottom_right = bottom_right
+        self.block_id = block_id
+        self.process_rank = process_rank
+        self.adjacent_blocks = adjacent_blocks
+
+
+    def __str__(self):
+        return f"Block {self.block_id} ({self.top_left}, {self.bottom_right}) - {self.factions} - Adjacent Blocks : {self.adjacent_blocks}"
+    
