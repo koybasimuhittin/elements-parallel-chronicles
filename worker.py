9e71001ee2500503d4bae3512077fd1380f9b400
OsmanYusufTosun <osyustos14@gmail.com>
2024-12-13 19:47:46 +0300

    deneme commit

--- worker.py
+++ worker.py
@@ -48,7 +48,7 @@ class Worker:
 
 
 
-                self.state = 0
+                self.s tate = 0
             elif (self.state == 3):
                 print(f"Worker {self.rank}: Only send data.")
                 self.state = 0
